f79a818c49478809eae50bc4faef92c53189a36e
Mikael Nenonen <mikael.nenonen@hive.fi>
2019-10-16 19:00:00 +0300

    Initial, working, and normal: 13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/15 21:50:58 by mnenonen          #+#    #+#             */
+/*   Updated: 2019/10/16 18:58:41 by mnenonen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb > 1)
+		return (ft_recursive_factorial(nb - 1) * nb);
+	return (nb);
+}
