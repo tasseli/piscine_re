0a6f682a2b982a4b2258f353c031cce3ceb47d8f
Mikael Nenonen <tassel@iki.fi>
2019-10-18 19:06:53 +0300

    ex20: Works, initial, normal

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/18 18:48:25 by mnenonen          #+#    #+#             */
+/*   Updated: 2019/10/18 19:06:27 by mnenonen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int		count;
+
+	count = 0;
+	while (*str != 0)
+	{
+		++count;
+		++str;
+	}
+	return (count);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		len;
+	int		i;
+	char	*newptr;
+
+	len = ft_strlen(src);
+	newptr = malloc((len + 1) * sizeof(char));
+	i = 0;
+	while (i < len)
+	{
+		newptr[i] = src[i];
+		++i;
+	}
+	return (newptr);
+}
