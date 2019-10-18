a2b655f1f39d05d1950196c61fc1d1f611e6f641
Mikael Nenonen <tassel@iki.fi>
2019-10-18 20:15:05 +0300

    Initial, working, normal ex21.

---
+++ ex21/ft_range.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/18 19:46:17 by mnenonen          #+#    #+#             */
+/*   Updated: 2019/10/18 20:14:17 by mnenonen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		*ft_range(int min, int max)
+{
+	int	*ptr;
+	int	i;
+
+	i = 0;
+	if (min > max)
+		return (NULL);
+	ptr = malloc((max - min + 1) * sizeof(int));
+	if (!ptr)
+		return (NULL);
+	while (i < max - min + 1)
+	{
+		ptr[i] = min + i;
+		++i;
+	}
+	return (ptr);
+}
