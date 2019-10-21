655bfdd5bdf7a50ef39136b5c61e19fa6637c882
Mikael Nenonen <tassel@iki.fi>
2019-10-21 20:46:55 +0300

    ex26: Working, normal

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -6,12 +6,22 @@
 /*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 20:26:41 by mnenonen          #+#    #+#             */
-/*   Updated: 2019/10/21 20:27:38 by mnenonen         ###   ########.fr       */
+/*   Updated: 2019/10/21 20:38:25 by mnenonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_count_if(char **tab, int (*f)(char*))
 {
+	int	i;
+	int	count;
 
-	return (0);
+	count = 0;
+	i = 0;
+	while (tab[i] != 0)
+	{
+		if (f(tab[i]) == 1)
+			++count;
+		++i;
+	}
+	return (count);
 }
