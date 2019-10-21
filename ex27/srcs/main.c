efd1b050d5c3bdc2490d1bfd3a4130baab2a3692
Mikael Nenonen <tassel@iki.fi>
2019-10-21 23:15:03 +0300

    ex27: found my working problem - two main.cs. Next - make file reading work

--- ex27/srcs/main.c
+++ ex27/srcs/main.c
@@ -6,11 +6,14 @@
 /*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 21:02:26 by mnenonen          #+#    #+#             */
-/*   Updated: 2019/10/21 21:07:19 by mnenonen         ###   ########.fr       */
+/*   Updated: 2019/10/21 23:13:37 by mnenonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 1
 
 void	ft_putstr(char *str)
 {
@@ -21,11 +24,38 @@ void	ft_putstr(char *str)
 	}
 }
 
+void	ft_puterror(char *str)
+{
+	while (*str != 0)
+	{
+		write(2, str, 1);
+		++str;
+	}
+}
+
 int		main(int argc, char **argv)
 {
+	int		fd;
+	char	buf[BUF_SIZE];
+
 	if (argc == 1)
-		ft_putstr("File name missing.\n");
+	{
+		ft_puterror("File name missing.\n");
+		return (-1);
+	}
 	if (argc >= 3)
-		ft_putstr("Too many arguments.\n");
+	{
+		ft_puterror("Too many arguments.\n");
+		return (-2);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		ft_puterror("Error opening the file for reading.\n");
+		return (-3);
+	}
+	read(fd, buf, BUF_SIZE);
+	ft_putstr(buf);
+	close(fd);
 	return (0);
 }
