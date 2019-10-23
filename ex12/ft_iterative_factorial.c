e89cdc3bb13cedd7eba06a12876ecf2849b0c2be
Mikael Nenonen <tassel@iki.fi>
2019-10-24 00:01:46 +0300

    ex12 normal

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 21:29:40 by mnenonen          #+#    #+#             */
-/*   Updated: 2019/10/23 22:27:09 by mnenonen         ###   ########.fr       */
+/*   Updated: 2019/10/24 00:01:30 by mnenonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		ft_iterative_factorial(int nb)
 			return (0);
 		--nb;
 	}
-	nb = (int) ans;
+	nb = (int)ans;
 	if (nb < 0)
 		return (0);
 	return (nb);
