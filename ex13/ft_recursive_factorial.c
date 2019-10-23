0fec62960285e0eb5401b7db72f62132490425da
Mikael Nenonen <tassel@iki.fi>
2019-10-23 22:34:49 +0300

    ex13: handling overflows by checking if result is less than 0

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,15 +6,24 @@
 /*   By: mnenonen <mnenonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 21:50:58 by mnenonen          #+#    #+#             */
-/*   Updated: 2019/10/16 19:14:14 by mnenonen         ###   ########.fr       */
+/*   Updated: 2019/10/23 22:34:19 by mnenonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
+	int ans;
+
 	if (nb > 1)
-		return (ft_recursive_factorial(nb - 1) * nb);
+	{
+		ans = ft_recursive_factorial(nb - 1) * nb;
+		if (ans < 0)
+			return 0;
+		return (ans);
+	}
 	else if (nb < 0)
 		return (0);
+	else if (nb == 0)
+		return (1);
 	return (nb);
 }
